18b70532765d6dc5c7cf87f407012b6f2641fe70
Leon-ER <leonerdesi2004@gmail.com>
2022-10-13 23:47:59 -0400

    Made the grid and hover effect

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
+// Makes the initial 16 containers
+let containers = document.querySelector('#container_main');
+let divs;
+for(i=0; i<16; i++){
+    const subContainers = document.createElement('div');
+    subContainers.id = "container" +i;
+    containers.appendChild(subContainers);
+    // makes the grids inside those containers
+    let container = document.querySelector(`#container${i}`);
+    for(let a=0; a<16; a++){
+        divs = document.createElement('div');
+        divs.classList.add('test');
+        container.appendChild(divs);
+    }
 
+} 
+const divClicked = document.querySelectorAll('.test')
+divClicked.forEach((divs) => {
+    divs.addEventListener('mouseover', () => {
+        divs.id = 'colored';
+        console.log('clicked')
+    })
+});
+
+const btn = document.querySelector('#clear');
+btn.addEventListener('click', () => {
+   
+});
